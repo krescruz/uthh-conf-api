495f6e100363e51921173d1abcf8ebec0326fdfb
krescruz <krescruz@gmail.com>
2015-11-29 21:48:19 -0600

    Demo get rest api.

---
+++ static/js/app.js
@@ -0,0 +1,44 @@
+(function(){
+
+angular
+	.module('uthhApp', [])
+	.config(['$httpProvider', '$interpolateProvider', function($httpProvider, $interpolateProvider){
+		$interpolateProvider.startSymbol('{[').endSymbol(']}');
+    	$httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
+	}]);
+
+})();
+(function(){
+	angular
+	.module('uthhApp')
+	.controller("UthhController", UthhController);
+
+	UthhController.$inject = ['$scope', 'UthhService'];
+
+	function UthhController($scope, uthhService){
+		var vm = this;
+		vm.hola = 'Hola mundo';
+
+		uthhService.getEquipos().then(function(response){
+			vm.equipos = response.data;
+		});
+	};
+})();
+
+(function(){
+	angular
+	.module('uthhApp')
+	.factory("UthhService", UthhService);
+
+	function UthhService($http){
+		var URL_BASE = "http://localhost:8000/api/";
+
+		var getEquipos = function(){
+			return $http.get(URL_BASE + 'equipos/');
+		};
+
+		return {
+			'getEquipos': getEquipos
+		};
+	};
+})();
